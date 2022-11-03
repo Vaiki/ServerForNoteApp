2ed4923231ccdf8393e812903612ebf7b3a94c08
vaiki <lifeotworld@gmail.com>
2022-11-03 22:13:06 +0300

    added task

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,9 @@ application {
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
-
+tasks.create("stage"){
+    dependsOn("installDist")
+}
 repositories {
     mavenCentral()
 }
